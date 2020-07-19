af665eb17ff192922db201000f3802b6c919299a
Ronald Pitts <ronpitts81@gmail.com>
2020-07-19 15:25:50 -0400

    adding first js

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
 # weather-app
 
+my weather app : file:///Users/ronaldpitts/Desktop/bootcamp_files/my-hw-folder/weather-app/index.html
+
 This app will show current and future weather within a 5 day range including date, temperature, windspeed and humidity.
 
 FIRST I create an html file titled index and set it up
-THEN 
+THEN I create the necessary divs, conatiners, ids, and classes I will be using on the page and linking to my CSS and JS files
+THEN I create the styling sheet and connect it using thee style tag and established classes and ids
+AFTER THAT I start on my JS script file 
+AND create a function for the search button click event and create a variables for the city input 
+NEXT I create if else statements for errors in case a valid city is not entered 
+THEN I make an ajax call to begin grabbing the information from the my weather API
+THEN within the ajax call I paste in my API key for the ajax call 
+NEXT I create a function to display the data in the browser in accordance to the recent call
+THEN I create the lines of code for how I want the information to be displayed 
---
+++ script.js
@@ -0,0 +1,41 @@
+
+
+$(document).ready(function(){
+
+    // click event for weather and grabbing weather data in after button is clicked
+     $('submitWeather').click(function(){
+
+        var city = $("#city").val(); 
+
+        if(city!=' '){
+            $.ajax({
+                 url: 'http://api.openweathermap.org/data/2.5/weather?q=' + city + "&units=imperial" + "&APPID=f20741f8dbd707c14900e7828566a854",
+                 type: "GET",
+                 datatype:"JSON",
+                 success: function(data){
+                     var widget = show(data);
+                     $("show").html(widget); 
+                      $("#city").val(' '); 
+                    
+                 }
+            });
+
+        }else {
+            $("#error").html("Field Needs A City");
+        }
+
+     });
+});
+
+// function that displays recently fetched data 
+
+function show(data){
+    return "<h2>Name: "+ data.city.name +"</h2>" + 
+           "<h2>date: "+ +"</h2>" +
+           "<h2>icon: "+ +"</h2>" +
+           "<h2>temperature: "+ data.temperature.value +"</h2>" +
+           "<h2>humidity: "+ data.humidity.value +"</h2>" +
+           "<h2>wind speed: "+ data.wind.speed.value +"</h2>" +
+           
+}
+
