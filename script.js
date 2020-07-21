b842b85b1d225769dc8c161d0d8c8b9cc0fc6906
Ronald Pitts <ronpitts81@gmail.com>
2020-07-21 16:15:37 -0400

    updated js

--- script.js
+++ script.js
@@ -3,21 +3,24 @@
 $(document).ready(function(){
 
     // click event for weather and grabbing weather data in after button is clicked
-     $('submitWeather').click(function(){
+     $('#submitWeather').click(function(){
 
         var city = $("#city").val(); 
 
         if(city!=' '){
             $.ajax({
                  url: 'http://api.openweathermap.org/data/2.5/weather?q=' + city + "&units=imperial" + "&APPID=f20741f8dbd707c14900e7828566a854",
-                 type: "GET",
+                 method: "GET",
                  datatype:"JSON",
                  success: function(data){
                      var widget = show(data);
-                     $("show").html(widget); 
+                     $("#show").html(widget); 
                       $("#city").val(' '); 
+
+                      var lastSearch = localStorage.getItem("lastSeacrh");
+                      localStorage.setItem("lastSearch", JSON.Stringify(response))
                     
-                 }
+                     }
             });
 
         }else {
@@ -30,7 +33,7 @@ $(document).ready(function(){
 // function that displays recently fetched data 
 
 function show(data){
-    return "<h2>Name: "+ data.city.name +"</h2>" + 
+    return "<h2>Name: "+ data.name +"</h2>" + 
            "<h2>todays date: "+ +"</h2>" +
            "<h2>description: <img 'src= http://openweathermap.org/img/wn/ "+ data.weather.icon +" @2x.png'> "+ data.weather.description +" </h2>" +
            "<h2>temperature: "+ data.temperature.value +"&deg;F</h2>" +
